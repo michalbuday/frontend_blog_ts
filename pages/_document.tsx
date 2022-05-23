012f6740ab48a6989ce72703248ab40998281262
michalbuday <dedobuday@gmail.com>
2022-05-23 11:57:48 +0200

    _document to typescript

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class AppDocument extends Document {
+    render() {
+        return (
+            <Html>
+            <Head>
+              {/* code for head in future */}
+            </Head>
+            <body>
+              <Main />
+              <NextScript />
+            </body>
+          </Html>
+        );
+    };
+};
+
+export default AppDocument;
